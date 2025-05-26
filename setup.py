e807df772042ceabb1dcd640f1d4c4e863e5fe6e
Maxfield Kassel <106034283+MaxfieldKassel@users.noreply.github.com>
2025-05-26 08:22:13 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 import site
 import setuptools
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
 # PEP517 workaround
 site.ENABLE_USER_SITE = True
 
-setuptools.setup()
+class bdist_wheel(_bdist_wheel):
+    """Mark wheel as non-pure (so it gets a linux_x86_64, macosx, etc. tag)."""
+    def finalize_options(self):
+        super().finalize_options()
+        self.root_is_pure = False
+
+setuptools.setup(
+    # pull metadata from setup.cfg
+    setup_requires=["wheel"],
+    cmdclass={"bdist_wheel": bdist_wheel},
+)
